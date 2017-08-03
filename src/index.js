9aea303227bf49f59c19ea2f165d3ed69a26a425
knipferrc <knipferrc@gmail.com>
2017-08-03 12:38:36 -0500

    export all

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-export { default as CookieStorage } from './CookieStorage'
+export { default as CookieVault } from './CookieVault'
+export { default as LocalVault } from './LocalVault'
+export { default as SessionVault } from './SessionVault'
